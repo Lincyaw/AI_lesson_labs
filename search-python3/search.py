162e7d8a69f417b79e26f52187a9cd8979ba7fdd
lincyaw <814750204@qq.com>
2020-10-17 19:48:27 +0800

    finish report

--- search-python3/search.py
+++ search-python3/search.py
@@ -4,7 +4,7 @@
 # educational purposes provided that (1) you do not distribute or publish
 # solutions, (2) you retain this notice, and (3) you provide clear
 # attribution to UC Berkeley, including a link to http://ai.berkeley.edu.
-# 
+#
 # Attribution Information: The Pacman AI projects were developed at UC Berkeley.
 # The core projects and autograders were primarily created by John DeNero
 # (denero@cs.berkeley.edu) and Dan Klein (klein@cs.berkeley.edu).
@@ -74,7 +74,7 @@ def tinyMazeSearch(problem):
     return [s, s, w, s, w, w, s, w]
 
 
-def depthFirstSearch(problem):
+def depthFirstSearch(problem: SearchProblem):
     """
     Search the deepest nodes in the search tree first.
 
@@ -89,87 +89,151 @@ def depthFirstSearch(problem):
     print("Start's successors:", problem.getSuccessors(problem.getStartState()))
     """
     "*** YOUR CODE HERE ***"
-    print("Start:", problem.getStartState())
-    print("Is the start a goal?", problem.isGoalState(problem.getStartState()))
-    print("Start's successors:", problem.getSuccessors(problem.getStartState()))
+    initState = (problem.getStartState(), "None")
+    invalidNode = None
+
+    path = util.Stack()
+    visited = util.Counter()
+    stack = util.Stack()
+
+    visited[initState[0]] = 1
+    stack.push(initState)
+
+    while not stack.isEmpty():
+        curr = stack.pop()
+
+        # 无效节点，用于标记节点层数
+        # 当取出一个无效节点时
+        # 表示result栈顶的节点没有子节点
+        # 或所有子节点已经访问过但无法找到可行解
+        if curr == invalidNode:
+            path.pop()
+            continue
+
+        path.push(curr)
+
+        if problem.isGoalState(curr[0]):
+            # 该节点为最终节点，结束循环
+            break
+        else:
+            # 当前节点不是最终节点
+            # 对当前节点进行扩展获得其子节点
+            nexts = problem.getSuccessors(curr[0])
+
+            # 节点被扩展，层数增加，推入一个invalid节点用于标记
+            stack.push(invalidNode)
+
+            # 选取未出现过的状态节点加入到队列中
+            for node in nexts:
+                if visited[node[0]] == 0:
+                    visited[node[0]] = 1
+                    stack.push((node[0], node[1]))
+
+    result = []
+    for i in path.list:
+        result.append(i[1])
+
+    if len(result) > 0:
+        result.pop(0)
+
+    return result
 
 
 def breadthFirstSearch(problem):
     """Search the shallowest nodes in the search tree first."""
     "*** YOUR CODE HERE ***"
-    s = problem.getStartState()  # 初始节点
-    closed = []  # 标记已经遍历过的节点，置为空
-    q = util.Queue()  # 建立队列来保存拓展到的各节点
-    q.push((s, []))
-    while not q.isEmpty():
-        state, path = q.pop()
-        if problem.isGoalState(state):  # 如果是目标状态，则返回当前路径，退出函数
-            return path
-        if state not in closed:
-            closed.append(state)  # 标记其为已经遍历过的状态
-            for node in problem.getSuccessors(state):
-                n_state = node[0]
-                direction = node[1]
-                if n_state not in closed:  # 如果后继状态未被遍历过，将其入队列
-                    q.push((n_state, path + [direction]))
-    return path
-    util.raiseNotDefined()
+    from game import Directions
+    STATE = 0
+    ACTION = 1
+    PRE_STATE = 2
+
+    initState = problem.getStartState()
+
+    queue = util.Queue()
+    visited = util.Counter()
+
+    # Every node is a triple, (nodeState, action, preNode) where
+    # 'nodeState' is the state for current node,
+    # 'action is how previous node goes to current node,
+    # 'preNode' is the previous node
+    queue.push((initState, "None", None))
+    visited[initState] = 1
+
+    lastNode = None
+    while not queue.isEmpty():
+        curr = queue.pop()
+
+        if problem.isGoalState(curr[STATE]):
+            lastNode = curr
+            break
+        else:
+            nexts = problem.getSuccessors(curr[STATE])
+            for node in nexts:
+                if visited[node[0]] == 0:
+                    # 未访问过
+                    visited[node[0]] = 1
+                    queue.push((node[0], node[1], curr))
+
+    result = []
+    if lastNode is not None:
+        while lastNode[PRE_STATE] is not None:
+            result.append(lastNode[ACTION])
+            lastNode = lastNode[PRE_STATE]
+
+    result.reverse()
+    return result
 
 
 def uniformCostSearch(problem):
     """Search the node of least total cost first."""
     "*** YOUR CODE HERE ***"
-    """Search the node of least total cost first."""
-    """ Dijkstra shorest path with null heuristic"""
-    # 扩展的是路径消耗g(n)最小的节点n,用优先队列来实现，对解的路径步数不关心，只关心路径总代价。
-    # 即使找到目标节点也不会结束，而是再检查新路径是不是要比老路径好，确实好，则丢弃老路径。
+    from game import Directions
+    STATE = 0
+    ACTION = 1
+    COST = 2
+    PRE_STATE = 3
 
-    start_point = problem.getStartState()  # 点
-    queue = util.PriorityQueueWithFunction(lambda x: x[2])  # 记录当前队列
-    queue.push((start_point, None, 0))  # 加入队列
-    cost = 0  # 现在的代价.
-    visited = []  # 标记是否记录
-    path = []  # 记录路径
-    parentSeq = {}
-    parentSeq[(start_point, None, 0)] = None
-    while queue.isEmpty() == False:
-        current_fullstate = queue.pop()  # 当前点
-        # print current_fullstate
-        if (problem.isGoalState(current_fullstate[0])):  # 目标状态
+    initState = problem.getStartState()
+
+    queue = util.PriorityQueueWithFunction(lambda e: e[COST])
+    visited = util.Counter()
+
+    # Every node is a triple, (nodeState, action, cost, preNode) where
+    # 'nodeState' is the state for current node,
+    # 'action is how previous node goes to current node,
+    # 'preNode' is the previous node
+    queue.push((initState, "None", 0, None))
+    visited[initState] = 1
+
+    lastNode = None
+    while not queue.isEmpty():
+        curr = queue.pop()
+
+        if problem.isGoalState(curr[STATE]):
+            lastNode = curr
             break
         else:
-            current_state = current_fullstate[0]
-            if current_state not in visited:
-                visited.append(current_state)
-            else:
-                continue
-            successors = problem.getSuccessors(current_state)  # 继承表后继, huoqu houji jiedian
-            for state in successors:
-                cost = current_fullstate[2] + state[2];
-                # print state,cost
-                if state[0] not in visited:
-                    queue.push((state[0], state[1], cost))
-                    # parentSeq[state] = current_fullstate
-                    parentSeq[(state[0], state[1])] = current_fullstate # record path
+            nexts = problem.getSuccessors(curr[STATE])
+            for node in nexts:
+                if visited[node[0]] == 0:
+                    # 未访问过
+                    visited[node[0]] = 1
+                    queue.push((node[0], node[1], node[2], curr))
 
-    child = current_fullstate  # final state
+    result = []
+    if lastNode is not None:
+        while lastNode[PRE_STATE] is not None:
+            result.append(lastNode[ACTION])
+            lastNode = lastNode[PRE_STATE]
 
-    while (child != None):
-        path.append(child[1])
-        if child[0] != start_point:
-            child = parentSeq[(child[0], child[1])]
-        else:
-            child = None
-    path.reverse()
-    return path[1:]
-
-    util.raiseNotDefined()
+    result.reverse()
+    return result
 
 
 def nullHeuristic(state, problem=None):
     """
     A heuristic function estimates the cost from the current state to the nearest
-    goal in the provided SearchProblem.  This heuristic is trivial.
+    goal in the provided SearchProblem. This heuristic is trivial.
     """
     return 0
 
